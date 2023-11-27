d591b4a652ce4552d08672ff5a7a956a339bb644
Messe225 <traoremesseci@gmail.com>
2023-11-27 11:29:03 +0000

    Téléchargement des données dans la base

---
+++ RestotelMap/load.py
@@ -0,0 +1,22 @@
+import geopandas as gpd
+from .models import *
+from django.contrib.gis.geos import MultiPoint, Point
+
+def charge_donnee(shp):
+    gdf = gpd.read_file(shp)
+    for index,row in gdf.iterrows():
+        catego_instance = Catego.objects.get(id = row['catego_id'])
+        local_instance = Local.objects.get(id = row['local_id'])
+        point = Point(row['longitude'], row['latitude'])
+        etab = Etabl(
+            nom = row['nom'],
+            catego = catego_instance,
+            telephone = row['telephone'],
+            local = local_instance,
+            latitude = row['latitude'],
+            longitude = row['longitude'],
+            geom = MultiPoint(point)
+        )
+        etab.save()
+        
+    
