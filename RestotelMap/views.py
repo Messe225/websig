c9da7da6da3527f429146576123b0185f1835fd1
Messe225 <traoremesseci@gmail.com>
2023-11-29 17:59:08 +0000

    codage du front-end

--- RestotelMap/views.py
+++ RestotelMap/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from django.template.context import Context
+from django.http import JsonResponse
+from .models import *
+#from django.db.models import Q
 
 # Create your views here.
+
+def accueil(request):
+    return render(request,'app/index.html')
+    
